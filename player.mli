091fcc3df031b7fc6aa663d5cdf4f46a9ccf83bf
maxim <mmc276@cornell.edu>
2021-03-24 19:12:00 -0400

    Player mli from max test

---
+++ player.mli
@@ -0,0 +1,11 @@
+(*The abstract type of values representing a player*)
+type player
+
+(*The type of player's hand*)
+type hand = Card.t list
+
+(*Initializes the player type*)
+val init : string -> int -> player
+
+(*[hand_to_string hand] Converts a [hand] to a string*)
+val hand_to_string : hand -> string
