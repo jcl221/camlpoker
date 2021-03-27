a77c08520c56278ec0b36aca64098a54caf501f4
maxim <mmc276@cornell.edu>
2021-03-27 15:46:56 -0400

    Fixed player_init

--- player.mli
+++ player.mli
@@ -5,7 +5,7 @@ type player
 type hand_type = Card.t list
 
 (* [player_init] initializes the [player] type*)
-val player_init : string -> int -> player
+val player_init : string -> hand_type -> player
 
 (* [hand_to_string hand] Converts a given players [player] hand to a string *)
 val hand_to_string : player -> string
