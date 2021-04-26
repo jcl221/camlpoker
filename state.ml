d7f856a3da26c42a47a40f490e47a5ccddf228f9
maxim <mmc276@cornell.edu>
2021-04-26 19:13:17 -0400

    Max's three functions

--- state.ml
+++ state.ml
@@ -19,43 +19,38 @@ type t = {
 
 exception Empty_Hand
 
-let init_state ids = failwith "Unimplemented"
-  (**let starting_table = Table.init_table () in
-  let init_player id =
-    {
-      id;
-      chips = 50;
-      bet = 0;
-      hand = Some (Table.deal_hand starting_table);
-      forfeited = false;
-    }
-  in
+let init_state ids =
+  let starting_table = Table.init_table () in
   {
-    players = List.map init_player ids;
-    action_queue = [];
+    players = List.map (Player.player_init starting_table) ids;
     active_bet = 0;
     table = starting_table;
     pot = 0;
-    winner = None;
-  }*)
+  }
 
-let deal_center st = failwith "Unimplemented"
-  (**st.table <- Table.place_center st.table*)
+let deal_center st = { st with table = Table.place_center st.table }
 
-let fold st id = failwith "Unimplemented"
-  (**let p = get_player st id in
-  p.forfeited <- true*)
+let fold st id =
+  let survey (p : Player.player) =
+    if p.name = id then { p with folded = true } else p
+  in
+  let updated_players = List.map survey st.players in
+  { st with players = updated_players }
 
-let bet st id amt = failwith "Unimplemented"
-  (**let p = get_player st id in
+(*match st.players with
+  |[] -> failwith ""
+  |h :: t -> if h.name = id then h.folded = true else *)
+
+(**let p = get_player st id in
   p.bet <- p.bet + amt;
   st.active_bet <- amt;
   st.pot <- st.pot + amt*)
+let bet st id amt = failwith "Unimplemented"
 
 let showdown st = failwith "Unimplemented"
+
 let active_bet st = st.active_bet
 
 let get_player st id = failwith "Unimplemented"
 
 let player_hands st = failwith "Unimplemented"
-
