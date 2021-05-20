0d43598f7725ea07a9a5c9c19f79158317cc2d18
maxim <mmc276@cornell.edu>
2021-05-20 16:29:26 -0400

    some ui changes

--- card.ml
+++ card.ml
@@ -21,17 +21,25 @@ let get_rank card = card.rank
 let string_of_card card =
   let r =
     match card.rank with
-    | 14 -> "Ace"
-    | 11 -> "Jack"
-    | 12 -> "Queen"
-    | 13 -> "King"
+    | 14 -> "A"
+    | 11 -> "J"
+    | 12 -> "Q"
+    | 13 -> "K"
     | x -> string_of_int x
   in
   let s =
     match card.suit with
-    | Hearts -> "Hearts"
-    | Diamonds -> "Diamonds"
-    | Spades -> "Spades"
-    | Clubs -> "Clubs"
+    | Hearts -> "♥"
+    | Diamonds -> "♦"
+    | Spades -> "♠"
+    | Clubs -> "♣"
   in
-  r ^ " of " ^ s
+
+  (*r ^ " of " ^ s *)
+  (*"\n----\n|   |\n|   |\n----"*)
+  (*"\n......\n.    .\n.    .\n.    .\n......"*)
+  "\n......\n|" ^ s ^ "   |\n|  " ^ r ^ " |\n|   " ^ s ^ "|\n......"
+
+(*"♥" "♦" "♠" "♣"*)
+
+(*"****\n*  *\n*  *\n*  *"*)
