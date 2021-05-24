33b2ef12a88da8f7d890fb46ba91e0e78f65d29d
James Li <jl3594@cornell.edu>
2021-05-24 00:34:52 -0700

    Add test cases | Add test plan description

--- test.ml
+++ test.ml
@@ -1,8 +1,38 @@
+(****************************************************************)
+(** Test Plan *)
+
+(** The testing of modules Card, Player, and Table largely consisted of
+    OUnit black-box tests, allowing us to ensure their adherence to
+    their specifications before integrating them in larger modules such
+    as State. The Command module was tested via glass-box tests that
+    ensured edge cases in user input could be parsed correctly.
+
+    The State module was tested using a combination of black-box tests,
+    which were primarily used for simple getters/setters, glass-box
+    testing of certain state transition functions (e.g., bet, fold,
+    all-in, reset), and manual testing through the command-line.
+
+    This manual component, in particular, was crucial in verifying that
+    state transitions were properly integrated in Main to facilitate
+    correct progression of the game. After each step in development, we
+    frequently noted down possible input/command combinations and tested
+    them against their expected results through the UI display. Later
+    introduction of an AI opponent was also tested similarly by testing
+    possible combinations of player input and AI responses.
+
+    This method provided us reliable verification of core functionality
+    in our base modules, while also allowing us to account for the many
+    different edge cases and nuances that could arise when actually
+    playing a poker match. *)
+
+(****************************************************************)
+
 open OUnit2
+open Card
 open Player
 open Table
-open Card
 open State
+open Command
 
 let pp_string s = "\"" ^ s ^ "\""
 
@@ -188,28 +218,21 @@ let full_deck_test name deck =
     done
   done
 
+let deal_hand_test name table =
+  name >:: fun _ ->
+  let c1, c2 = Table.deal_hand table in
+  assert (
+    Card.get_rank c1 <> Card.get_rank c2
+    || Card.get_suit c1 <> Card.get_suit c2)
+
 (** [raises_exn_test name f exn] is an OUnit test named [name] asserting
     that calling [f ()] raises exception [exn]. *)
 let raise_exn_test name exn f = name >:: fun _ -> assert_raises exn f
 
-(** [new_card_test name table] is the OUnit test named [name], asserting
-    that [new_table table] is a table:
-
-    1) with a deck that does not contain the most recent community card
-    2) whose most recent community card was in the table to draw from,
-    [table] *)
-
-(* let new_card_test name table = name >:: fun _ -> let new_table =
-   init_table in let drawn = match snd new_table with | Some (h :: _) ->
-   h | Some [] | None -> failwith "No card drawn" in let old_deck = fst
-   table in let new_deck = fst new_table in assert (contains_card drawn
-   old_deck); assert (not (contains_card drawn new_deck)) *)
 (* Test decks *)
 let start_deck = init_deck ()
 
 let shuffled_deck = shuffle start_deck
-
-let oversized = { suit = Hearts; rank = 1 } :: start_deck
 
 let one_card_deck = [ { suit = Hearts; rank = 1 } ]
 
@@ -225,10 +248,116 @@ let table_tests =
     deck_size_test "shuffling preserves deck size" shuffled_deck 52;
     full_deck_test "shuffling preserves elements of a deck"
       shuffled_deck;
-    (*raise_exn_test "oversized deck is an invalid starting deck"
-      Invalid_Deck (fun _ -> assert_valid_start oversized);
-      raise_exn_test "deck with one card is an invalid starting deck"
-      Invalid_Deck (fun _ -> assert_valid_start one_card_deck);*)
+    deal_hand_test "deal out two different cards for a hand" start_table;
+  ]
+
+(****************************************************************)
+(* State *)
+(****************************************************************)
+
+let diff_hands_test name id1 id2 st =
+  name >:: fun _ ->
+  let card_1a, card_1b = (State.get_player id1 st).hand in
+  let card_2a, card_2b = (State.get_player id2 st).hand in
+  assert (
+    card_1a <> card_2a && card_1a <> card_2b && card_1b <> card_2a
+    && card_1b <> card_2b)
+
+let chips_test name id st expected =
+  name >:: fun _ ->
+  assert_equal expected (State.chips id st) ~printer:string_of_int
+
+let active_bet_test name st expected =
+  name >:: fun _ ->
+  assert_equal expected (State.active_bet st) ~printer:string_of_int
+
+let active_players_test name st expected =
+  name >:: fun _ -> assert_equal expected (State.active_players st)
+
+let locked_test name st expected =
+  name >:: fun _ ->
+  assert_equal expected (State.locked st) ~printer:string_of_bool
+
+let init_chips = 200
+
+let new_match = State.init_state [ "dum1"; "dum2" ]
+
+let bet1 = new_match |> State.bet "dum1" 0 50
+
+let bet2 = bet1 |> State.bet "dum2" 0 100
+
+let fold1 = bet2 |> State.fold "dum1"
+
+let reset = fold1 |> State.reset [ "dum2" ]
+
+let showdown = fold1 |> State.deal_center 5 |> State.showdown
+
+let all_in1 = reset |> State.all_in "dum2" 0
+
+let all_in2 = all_in1 |> State.all_in "dum1" 0
+
+let all_in3 =
+  new_match |> State.all_in "dum1" 0 |> State.all_in "dum2" 0
+
+let state_tests =
+  [
+    active_bet_test "init state starts w/ active bet of 0" new_match 0;
+    active_players_test "all players in init state are active" new_match
+      [ "dum1"; "dum2" ];
+    chips_test "player 1 starts w/ 200 chips" "dum1" new_match 200;
+    chips_test "player 2 starts w/ 200 chips" "dum2" new_match 200;
+    diff_hands_test "all players in init state have different hands"
+      "dum1" "dum2" new_match;
+    active_bet_test "opening bet raises active bet" bet1 50;
+    chips_test "betting subtracts chips from player" "dum1" bet1 150;
+    active_bet_test "raising increases active bet" bet2 100;
+    active_bet_test "folding doesn't affect active bet" fold1 100;
+    active_players_test "folded players aren't active" fold1 [ "dum2" ];
+    active_bet_test "resetting brings active bet back to 0" reset 0;
+    active_players_test "all players in reset state are active" reset
+      [ "dum1"; "dum2" ];
+    chips_test "distribution of pot to winner via reset" "dum2" reset
+      250;
+    chips_test "none of the pot is given to loser during reset" "dum1"
+      reset 150;
+    active_bet_test "showdown resets active bet back to 0" showdown 0;
+    active_players_test "all players after showdown are active" showdown
+      [ "dum1"; "dum2" ];
+    active_bet_test "if all-in amt > active bet, active bet increases"
+      all_in1 250;
+    chips_test "all-in depletes chips" "dum2" all_in1 0;
+    locked_test "all-in > active bet doesn't lock betting" all_in1 false;
+    active_bet_test "if all-in amt < active bet, active bet decreases"
+      all_in2 150;
+    chips_test "all-in depletes chips" "dum1" all_in2 0;
+    chips_test "redistribution of pot from all-in" "dum2" all_in2 100;
+    locked_test "all-in < active bet locks betting" all_in2 true;
+    locked_test "all-in = active bet locks betting" all_in3 true;
+    locked_test "lock betting" (State.lock new_match) true;
+  ]
+
+(****************************************************************)
+(* Command *)
+(****************************************************************)
+let parse_test name str expected =
+  name >:: fun _ ->
+  assert_equal expected (Command.parse str)
+    ~printer:Command.string_of_cmd
+
+let command_tests =
+  [
+    parse_test "parse bet cmd" "bet 50" (Bet 50);
+    parse_test "parse raise cmd" "raise 50" (Raise 50);
+    parse_test "parse check cmd" "Check " Check;
+    parse_test "parse call cmd" "cALL" Call;
+    parse_test "parse fold cmd" "    fold    " Fold;
+    parse_test "parse all-in cmd" "All in" AllIn;
+    parse_test "invalid bet cmd" "Bet" Invalid;
+    parse_test "invalid bet arg" "Bet x" Invalid;
+    parse_test "invalid raise cmd" "Raise child" Invalid;
+    parse_test "invalid spacing" "b et" Invalid;
+    parse_test "wrong num of keywords" "bet 50 50" Invalid;
+    parse_test "gibberish" "gibberish" Invalid;
   ]
 
 (****************************************************************)
@@ -390,14 +519,15 @@ let hand_rank_tests =
     compare_hands_test "pair vs. 2pair" pair2 two_pair1 (-1);
     compare_hands_test "higher 2pair vs. lower" two_pair2 two_pair1 1;
     compare_hands_test "3ofkind vs. 2pair" three_of_kind two_pair1 1;
-    compare_hands_test "3ofkind vs. straight" three_of_kind straight1 (-1);
+    compare_hands_test "3ofkind vs. straight" three_of_kind straight1
+      (-1);
     compare_hands_test "straight tie" straight1 straight2 0;
     compare_hands_test "straight vs. pair" straight1 pair1 1;
     compare_hands_test "flush vs. straight" flush1 straight1 1;
     compare_hands_test "higher flush vs. lower" flush1 flush2 1;
     compare_hands_test "full house vs. flush" full_house flush1 1;
     compare_hands_test "full house vs. four of kind" full_house
-      four_of_kind1 (-1); 
+      four_of_kind1 (-1);
     compare_hands_test "four of kind higher vs. lower" four_of_kind1
       four_of_kind2 (-1);
     compare_hands_test "straight flush vs. four of kind" straight_flush
@@ -408,6 +538,13 @@ let hand_rank_tests =
 let suite =
   "test suite for MS1"
   >::: List.flatten
-         [ player_tests; card_mod_tests; table_tests; hand_rank_tests ]
+         [
+           player_tests;
+           card_mod_tests;
+           table_tests;
+           state_tests;
+           command_tests;
+           hand_rank_tests;
+         ]
 
 let _ = run_test_tt_main suite
